eaa6367db9581b20c52882d0da920eae3394eaeb
Wang Ruiyi <58590050+ray923@users.noreply.github.com>
2025-04-08 01:47:09 +1000

    basic build
    adding basic library

--- FrontEnd/src/utils/helper.js
+++
@@ -1,7 +0,0 @@
-export function HTMLDecode(text) { 
-  var temp = document.createElement("div"); 
-  temp.innerHTML = text; 
-  var output = temp.innerText || temp.textContent; 
-  temp = null; 
-  return output; 
-} 
